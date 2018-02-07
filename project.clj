0b5614e04b6571ff2e5e3421c75dc43364f16d19
Michael McDermott <michael.mcdermott@jbhunt.com>
2018-02-07 16:54:12 -0600

    Added CSV library; began hacking on simple code to load a spreadsheet to interpret.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject sozu "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Sozu is a spreadsheet interpreter for the command line."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.4"]]
   :main ^:skip-aot sozu.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/sozu/core.clj
+++ src/sozu/core.clj
@@ -1,5 +1,25 @@
 (ns sozu.core
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io])
   (:gen-class))
+
+; TODO: dispose of readers & writers
+(defn- load-sozu-file [path]
+ (let [parsed (with-open [reader (io/reader path)] 
+                   (doall (csv/read-csv reader))) ]
+ {
+     :values parsed
+     :display parsed
+     :dependencies nil
+ }
+ )
+)
+
+(defn- sheet->csvfile [path sheet]
+ (with-open [writer (io/writer path)]
+  (csv/write-csv writer (:display sheet))
+ )
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
